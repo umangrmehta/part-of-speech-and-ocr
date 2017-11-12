33465c8a2176635d20f231e90db99eaa65189d4a
Ayesha Bhimdiwala <aybhimdi@iu.edu>
2017-11-12 00:34:32 -0500

    OCR - letter dict update

--- part2/ocr.py
+++ part2/ocr.py
@@ -22,7 +22,7 @@ def load_letters(fname):
     print int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH
     result = []
     for x_beg in range(0, int(x_size / CHARACTER_WIDTH) * CHARACTER_WIDTH, CHARACTER_WIDTH):
-        result += [ [ "".join([ '*' if px[x, y] < 1 else ' ' for x in range(x_beg, x_beg+CHARACTER_WIDTH) ]) for y in range(0, CHARACTER_HEIGHT) ], ]
+        result += [ [ "".join([ '1' if px[x, y] < 1 else '0' for x in range(x_beg, x_beg+CHARACTER_WIDTH) ]) for y in range(0, CHARACTER_HEIGHT) ], ]
     return result
 
 def load_training_letters(fname):
@@ -30,14 +30,30 @@ def load_training_letters(fname):
     letter_images = load_letters(fname)
     return { TRAIN_LETTERS[i]: letter_images[i] for i in range(0, len(TRAIN_LETTERS) ) }
 
+
 #####
 # main program
 (train_img_fname, train_txt_fname, test_img_fname) = sys.argv[1:]
 train_letters = load_training_letters(train_img_fname)
 test_letters = load_letters(test_img_fname)
-
+#print train_letters['a']
 ## Below is just some sample code to show you how the functions above work. 
 # You can delete them and put your own code here!
+letPattrn = {}
+
+
+train_arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789(),.-!?\"' "
+str=""
+count = 0
+for i in train_arr:
+    letter = train_letters[i]
+    letterList = []
+    for x in range(len(letter)):
+        for y in range(len(letter[0])):
+                letterList.append(letter[x][y])
+    letPattrn[i] = letterList
+
+print letPattrn['a']
 
 
 # Each training letter is now stored as a list of characters, where black
@@ -47,7 +63,7 @@ print "\n".join([ r for r in train_letters['a'] ])
 
 # Same with test letters. Here's what the third letter of the test data
 #  looks like:
-print "\n".join([ r for r in test_letters[2] ])
+print "\n".join([ r for r in test_letters[0] ])
 
 
 
